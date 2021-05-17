50ba76074e87edde9b94dcae6119ce2e2196a193
boshtechnologies <boshtech0@gmail.com>
2021-05-17 07:41:46 -0700

    commit

--- src/component/dashboard/cards.jsx
+++ src/component/dashboard/cards.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Chart from './chart';
+import FeedBacks from './feedBackList';
 
 function Cards() {
 
@@ -31,13 +33,25 @@ function Cards() {
 
   ]);
 
+  const [tab, setTab] = useState('chart');
+   const [show, setShow] = useState(true);
+
+    const feeds =(e)=>{
+      setTab('feedbacks');
+      setShow(false);
+    }
+    const chart =(e)=>{
+      setTab('chart');
+      setShow(true);
+    }
+
   return (
     <div className="jumbotron my-2" >
       <div className=" card-columns   ">
                     { list.map(({name, color})=>{
 
                         return(
-               <div key={name} className={`card    rounded-4  ${color} my-2  py-5 `}>
+               <div key={name} className={`card  rounded-4  ${color} my-2  py-5 `}>
 
                 <div className=" card-body  text-center">
 
@@ -50,9 +64,13 @@ function Cards() {
                 </div>
                         )
                     }) }
+      </div>
+      <div className='bg-white my-3 py-1 border-4 rounded'>
+      <span className='mx-3' style={tab !== 'chart' ?{color:'gray', fontWeight:'500'} :{color:'red', borderBottom:'2px solid red', fontWeight:'500'}} onClick={chart}>Chart</span>   <span style={tab !== 'feedbacks' ?{color:'gray', fontWeight:'500'} :{color:'red', borderBottom:'2px solid red', fontWeight:'500'}} onClick={feeds} className='mx-3'>Recent Feedback</span>
+      </div>
+      <div className='my-2'>
 
-
-
+      {show ? < Chart />  : <FeedBacks />}
 
       </div>
     </div>
