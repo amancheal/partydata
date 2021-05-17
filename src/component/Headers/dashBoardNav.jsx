2db2fefd0894c701e0c4d44f8b399330465e79f8
boshtechnologies <boshtech0@gmail.com>
2021-05-17 12:35:22 -0700

    doneDash

--- src/component/Headers/dashBoardNav.jsx
+++ src/component/Headers/dashBoardNav.jsx
@@ -23,7 +23,7 @@ let openNav =()=>{
             <div id='page-content-wrapper  '>
             <nav className="navbar  navbar-expand-lg  bg-transparent navbar-light py-4 px-4 ">
                 <div className='d-flex align-items-center'>
-            <i style={{fontSize:'30px',cursor:'pointer'}}   onClick={openNav } className='fas fa-align-left  primary-text fs-4 me-3'  id='menu-toggle'></i>
+            <i style={{fontSize:'30px',cursor:'pointer'}}   onClick={openNav } className='far fa-bars  primary-text fs-4 me-3'  id='menu-toggle'></i>
             <span  className='dash navbar-brand '>{whattoshow}</span>
             </div>
            </nav>
