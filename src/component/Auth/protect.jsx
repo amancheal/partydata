e01d4d284ea8ed1b10b39f8f277210df5a30b470
boshtechnologies <boshtech0@gmail.com>
2021-05-21 15:00:15 -0700

    commit

--- src/component/Auth/protect.jsx
+++ src/component/Auth/protect.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Redirect , Route } from 'react-router-dom';
 
 function Protect({isAuth: isAuth, component: Component, ...rest}){
-
+        console.log(isAuth)
   return <Route {...rest} render={({location})=> {
       if(isAuth === 'Go'){
+        console.log(isAuth)
         return  <Component />
       }else {
          return <Redirect to={{pathname:'/', state:{from: location} } }  />
