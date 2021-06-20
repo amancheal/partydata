951d03a4b5686625e13506b80f475a5562343e14
Opeyemi Olorunfemi <itsopeyemi@gmail.com>
2021-06-20 09:35:19 +0100

    updated api url

--- src/component/stackholders/stakeHoldersCard.jsx
+++ src/component/stackholders/stakeHoldersCard.jsx
@@ -11,7 +11,7 @@ function StakeHoldersCards({getWhatToShow}){
 
     useEffect(()=>{
 
-        axios.get('http://41.190.25.21:3001/kwara',{
+        axios.get('https://quiet-temple-20315.herokuapp.com/kwara',{
             headers:{
                 "Content-Type": "application/json",
                  Authorization: `Bearer ${localStorage.getItem("token")}`,
